544de747a74e606fea80ed5923344378d25fe3b7
thekirankumardash <thekirankumardash@gmail.com>
2016-04-30 08:20:40 +0530

    03_02-Creating a custom front page template file

---
+++ front-page.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The custom template for the one-page style front page. Kicks in automatically.
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<div>Hellp! I'm the front-page.php file.</div>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
