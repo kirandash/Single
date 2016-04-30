192e3b58d7ea3cbc4470cf4708f96301984c6cb6
thekirankumardash <thekirankumardash@gmail.com>
2016-04-30 23:04:59 +0530

    03_05-Setting up styles for the front page

--- front-page.php
+++ front-page.php
@@ -5,42 +5,52 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area lander-page">
 		<main id="main" class="site-main" role="main">
 
 			<section id="call-to-action">
-            	<div class="front-left">
-                	<h2 class="section-title">Call to Action</h2>
-                </div>
-                <div class="front-right">
-                	Form
+            	<div class="indent">
+                    <div class="front-left">
+                        <h2 class="section-title">Call to Action</h2>
+                    </div>
+                    <div class="front-right">
+                        Form
+                    </div>
                 </div>
             </section>
             
             <section id="testimonials">
-            	Testimonials	
+            	<div class="indent">
+                	Testimonials	
+                </div>
             </section>			
             
             <section id="services">
-            	<h2 class="section-title">Services</h2>
-                some text goes here
-                <ul>
-                	<li>Item one</li>
-                    <li>Item two</li>
-                    <li>Item three</li>
-                </ul>
+            	<div class="indent">
+                    <h2 class="section-title">Services</h2>
+                    some text goes here
+                    <ul>
+                        <li>Item one</li>
+                        <li>Item two</li>
+                        <li>Item three</li>
+                    </ul>
+                </div>
             </section>
             
             <section id="meet">
-            	<h2 class="section-title">Meet the Doctor</h2>
+            	<div class="indent">
+                	<h2 class="section-title">Meet the Doctor</h2>
+                </div>
             </section>
             
             <section id="contact">
-            	<div class="front-left">
-                	<h2 class="section-title">Contact us</h2>
-                </div>
-                <div class="front-right">
-                	Address
+            	<div class="indent">
+                    <div class="front-left">
+                        <h2 class="section-title">Contact us</h2>
+                    </div>
+                    <div class="front-right">
+                        Address
+                    </div>
                 </div>
             </section>
 
