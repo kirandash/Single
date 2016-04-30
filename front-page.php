8db85eda93c7e3f143ba5167d7444d43afca72c2
thekirankumardash <thekirankumardash@gmail.com>
2016-04-30 21:39:54 +0530

    03_03-Adding dummy content to the front-page.php file

--- front-page.php
+++ front-page.php
@@ -8,10 +8,43 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<div>Hellp! I'm the front-page.php file.</div>
+			<section id="call-to-action">
+            	<div class="front-left">
+                	<h2 class="section-title">Call to Action</h2>
+                </div>
+                <div class="front-right">
+                	Form
+                </div>
+            </section>
+            
+            <section id="testimonials">
+            	Testimonials	
+            </section>			
+            
+            <section id="services">
+            	<h2 class="section-title">Services</h2>
+                some text goes here
+                <ul>
+                	<li>Item one</li>
+                    <li>Item two</li>
+                    <li>Item three</li>
+                </ul>
+            </section>
+            
+            <section id="meet">
+            	<h2 class="section-title">Meet the Doctor</h2>
+            </section>
+            
+            <section id="contact">
+            	<div class="front-left">
+                	<h2 class="section-title">Contact us</h2>
+                </div>
+                <div class="front-right">
+                	Address
+                </div>
+            </section>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
