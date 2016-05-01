01da9afe20284bb5458ddfdf2702d320669b491b
thekirankumardash <thekirankumardash@gmail.com>
2016-05-01 09:22:47 +0530

    04_03-Using a custom loop to retrieve Services page children

--- front-page.php
+++ front-page.php
@@ -39,7 +39,8 @@ get_header(); ?>
                 <div class="indent clear">
                     <?php 
                     $query = new WP_Query( 'pagename=services' );
-            
+            		$services_id = $query->queried_object->ID;
+					
                     // The Loop
                     if ( $query->have_posts() ) {
                         while ( $query->have_posts() ) {
@@ -53,6 +54,33 @@ get_header(); ?>
             
                     /* Restore original Post Data */
                     wp_reset_postdata();
+					
+					$args = array(
+						'post_type' => 'page',
+						'post_parent' => $services_id
+					);
+					$services_query = new WP_Query( $args );
+					
+					// The Loop
+					if ( $services_query->have_posts() ) {
+						
+						echo '<ul class="services-list">';
+						while ( $services_query->have_posts() ) {
+							$services_query->the_post();
+							echo '<li class="clear">';
+							echo '<a href="' . get_permalink() . '" title="Learn more about ' . get_the_title() . '">';
+							echo '<h3 class="services-title">' . get_the_title() . '</h3>';
+							echo '</a>';
+							echo '<div class="services-lede">';
+							the_content('Read More...');
+							echo '</div>';
+							echo '</li>';
+						}
+						echo '</ul>';
+					}
+			
+					/* Restore original Post Data */
+					wp_reset_postdata();
                     ?>
                 </div><!-- .indent -->
             </section><!-- #services -->
